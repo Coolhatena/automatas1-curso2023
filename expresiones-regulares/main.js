e207b9bcc853be8aa15135825a5fef7b67c02122
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-09-17 16:07:31 -0700

    Add form for Regex assert

--- expresiones-regulares/main.js
+++ expresiones-regulares/main.js
@@ -1,12 +1,39 @@
-import './style.css';
+import './style.css'
+import assertRegex from './assertRegex';
 
-const cadena = '394858';
-let expresionRegular = /[01]+/;
-const parrafo = document.createElement('p');
-
-if (expresionRegular.test(cadena)) {
-  parrafo.textContent = 'Si hizo match ✅';
-} else {
-  parrafo.textContent = 'No hizo match 🚩';
+const handleButtonClick = () => {	
+	const paragraphText = assertRegex(stringInput.value, regexInput.value)
+	paragraph.textContent = paragraphText;
 }
-document.body.appendChild(parrafo);
+
+const mainContainer = document.createElement("div");
+mainContainer.classList.add("main-container");
+
+const title = document.createElement("h1");
+title.innerHTML = "Practica: Regex"
+
+const regexInput = document.createElement("input");
+regexInput.type = "text";
+regexInput.placeholder = "Regex"
+
+const stringInput = document.createElement("textarea");
+stringInput.placeholder = "Texto a probar"
+
+
+
+const AssertRegexButton = document.createElement("button");
+AssertRegexButton.innerHTML = 'Probar Regex'
+AssertRegexButton.type = "button";
+AssertRegexButton.onclick = handleButtonClick
+
+const paragraph = document.createElement('p');
+paragraph.textContent = "";
+
+
+mainContainer.appendChild(title);
+mainContainer.appendChild(regexInput);
+mainContainer.appendChild(stringInput);
+mainContainer.appendChild(AssertRegexButton);
+mainContainer.appendChild(paragraph);
+
+document.body.appendChild(mainContainer);
