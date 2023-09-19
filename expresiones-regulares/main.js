2999c0559bb4028a0fcba4f9b92d759d5526b164
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-09-18 22:40:48 -0700

    Add elements to support custom flags & changes on the way regex verification is treated

--- expresiones-regulares/main.js
+++ expresiones-regulares/main.js
@@ -1,9 +1,27 @@
 import './style.css'
 import assertRegex from './assertRegex';
 
-const handleButtonClick = () => {	
-	const paragraphText = assertRegex(stringInput.value, regexInput.value)
+const displayData = (isMatch, matches) => {
+	const paragraphText = ( isMatch ) ? 'Se encontró el patrón✅' : 'No se encontró el patrón ❌';
 	paragraph.textContent = paragraphText;
+
+	if ( isMatch ) {
+		let matchText = "Coincidencias: ";
+		matches.forEach( (match, i, array) => {
+			matchText += `${match}${(i < array.length - 1)? "," : ""} `
+		});
+		resultInput.textContent = matchText;
+	} 
+	else {
+		resultInput.textContent = "";
+	}
+}
+
+const handleButtonClick = () => {	
+	const regexResult = assertRegex(stringInput.value, regexInput.value, regexFlagsInput.value);
+	const [isMatch, matches] = regexResult;
+	displayData(isMatch, matches);
+	console.log(matches);
 }
 
 const mainContainer = document.createElement("div");
@@ -16,10 +34,12 @@ const regexInput = document.createElement("input");
 regexInput.type = "text";
 regexInput.placeholder = "Regex"
 
+const regexFlagsInput = document.createElement("input");
+regexFlagsInput.type = "text";
+regexFlagsInput.placeholder = "Regex flags (Ej. g, i, gi)"
+
 const stringInput = document.createElement("textarea");
 stringInput.placeholder = "Texto a probar"
-
-
 
 const AssertRegexButton = document.createElement("button");
 AssertRegexButton.innerHTML = 'Probar Regex'
@@ -29,11 +49,18 @@ AssertRegexButton.onclick = handleButtonClick
 const paragraph = document.createElement('p');
 paragraph.textContent = "";
 
+const resultInput = document.createElement("textarea");
+resultInput.readOnly = true;
+resultInput.classList.add("results")
+resultInput.placeholder = "Las coincidencias apareceran aqui"
+
 
 mainContainer.appendChild(title);
 mainContainer.appendChild(regexInput);
+mainContainer.appendChild(regexFlagsInput);
 mainContainer.appendChild(stringInput);
 mainContainer.appendChild(AssertRegexButton);
 mainContainer.appendChild(paragraph);
+mainContainer.appendChild(resultInput);
 
 document.body.appendChild(mainContainer);
