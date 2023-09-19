603ce5da9290efac9739cc165f95e59b13e6c668
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-09-18 22:39:36 -0700

    Change return logic & add suport for flags

--- expresiones-regulares/assertRegex.js
+++ expresiones-regulares/assertRegex.js
@@ -1,7 +1,17 @@
-const assertRegex = (string, pattern) => {
-	const regex = new RegExp(pattern);
-	return ( regex.test(string) ) ? 'Se encontró el patrón✅' : 'No se encontró el patrón ❌';
-	
+const theresInvalidFlags = (flagsString) => {
+	const flagChecker = /[^igmsuy]/;
+	const isNotAFlag = flagChecker.test(flagsString)
+	return isNotAFlag 
+}
+
+const assertRegex = (string, pattern, flags) => {
+	if ( theresInvalidFlags(flags) ){
+		alert("Se introdujeron flags invalidas, verifiquelas nuevamente");
+		return [false, ""]
+	}
+	const regex = new RegExp(pattern, flags);
+	const result = [regex.test(string), string.match(regex)];
+	return result;
 };
 
 export default assertRegex;
