ba666ff98602199476eaeef54e04f3e49a273920
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-10-16 12:21:36 -0700

    Change home

--- finite-automata/app/page.js
+++ finite-automata/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-	<div>Home Page</div>
+	<div className='text-center text-2xl p-3'>Seleccione un ejercicio para comenzar</div>
   )
 }
 
